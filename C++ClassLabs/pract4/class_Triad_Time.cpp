75c304d36eef08269544deee68e27a371e38137a
Irnil <32452526+Irnil@users.noreply.github.com>
2018-12-13 02:24:30 +0300

    Triad funcs added: Enter, Show

--- C++ClassLabs/pract4/class_Triad_Time.cpp
+++ C++ClassLabs/pract4/class_Triad_Time.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <sstream>
 
 using namespace std;
 
@@ -37,14 +38,49 @@ class Time{
 };
 
 
+//----------------------------------FREE_FUNCTIONS---------------------------------------------------------------------------------------------------
+
+std::string toString(int valInt)
+{
+	std::ostringstream putStr;
+	putStr << valInt;
+	return putStr.str();
+}
+
+void Read(string U, int &T){
+			
+	while (true){
+		
+		cout << U << "";
+		cin >> T;
+		
+		if (cin.peek() == '\n' and T>0){
+			cin.get();
+			break;
+			system("cls");
+		}
+		else{
+			system("cls");
+			cout << "Wrong! Value must be a number and >0!" << endl;
+			cout<<""<<endl;
+			cin.clear();
+			while (cin.get() != '\n') {}
+		}
+	}
+}
+
+
 //----------------------------------CLASS_TRIAD_METHODS_REALIZE--------------------------------------------------------------------------------------
 
 void Triad :: Enter(){
-	
+	Read("Enter first number: ", tri1);
+	Read("Enter second number: ", tri2);
+	Read("Enter third number: ", tri3);
+	system("cls");
 }
 
 void Triad :: Show(){
-	
+	cout<<tri1<<" | "<<tri2<<" | "<<tri3<<endl;
 }
 
 void Triad :: Compare(){
@@ -78,38 +114,42 @@ void Time :: Compare(){
 }
 
 
-//----------------------------------FREE_FUNCTIONS---------------------------------------------------------------------------------------------------
-
-std::string toString(int valInt)
-{
-	std::ostringstream putStr;
-	putStr << valInt;
-	return putStr.str();
-}
-
-void Read(string U, int &T){
-			
-	while (true){
-		
-		cout << U << "";
-		cin >> T;
-		
-		if (cin.peek() == '\n'){
-			cin.get();
-			break;
-		}
-		else{
-			cout << "Wrong input! Try again." << endl;
-			cout<<""<<endl;
-			cin.clear();
-			while (cin.get() != '\n') {}
-		}
-	}
-}
-
 //------------------------------------MAIN_BLOCK-----------------------------------------------------------------------------------------------------
 
 int main(){
+	int key;
+	Triad GO;
+	
+	GO.Enter();
+	
+for( ; ; ){
+
+	cout<<""<<endl;
+	cout<<"Select action:"<<endl;
+	cout<<"1. Show triad"<<endl;
+	cout<<"7. Exit programm"<<endl;
+	
+	while(true){
+
+				cin>>key;
+				cout<<""<<endl;
+		
+				if(cin.peek()=='\n'){
+					cin.get();
+					break;
+				}
+				else{
+					cout<<"Wrong input! Select a correct action."<<endl;
+					cin.clear();	
+				while(cin.get()!='\n'){}
+				}
+			}
+
+	switch(key){
+		case 1: GO.Show(); break; 
+		case 7: exit(1);
+		default:cout<<"Wrong! Select a correct action."<<endl; break;}
+	}
 	
 }
 
